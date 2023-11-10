81b57f906904590dd05fe542af3826ef20fff2dc
Jefferson NGUECHOUM <jeffersonnguechoum@Jeffersons-MacBook-Pro.local>
2023-11-10 11:51:40 +0100

    if instruction

---
+++ If_Anweisung/Answer.java
@@ -0,0 +1,23 @@
+
+public class Answer {
+	/*
+	 * Die main Methode verschiedene Aufrufe testen
+	 */
+	public static void main(String[] args) {
+		System.out.println(Answer.theAnswer(42));//sollte true zurueckgeben
+		System.out.println(Answer.theAnswer(27));//sollte false zurueckgeben
+	}
+	
+	/*
+	 * Diese Methode soll zurueckgeben, ob der �bergeben Parameter 42 ist.
+	 * Wenn dies der Fall ist, dann soll true zur�ckgegeben werden, ansonsten false.
+	 */
+	public static boolean theAnswer(int zahl)
+	{	if (zahl == 42)
+			{return true ;}
+		else
+		//Hier beginnt der zu bearbeitende Bereich
+			return false;
+		//Hier endet der zu bearbeitende Bereich
+	}
+}
